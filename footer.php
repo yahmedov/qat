ba1d121b6804602f0dc29e9b151a20e55c614ed7
yahmedov <ahmedov.yuksel@gmail.com>
2017-04-06 00:26:41 +0300

    the simple looking footer
    this is the simple cool footer

---
+++ footer.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ *	The template for displaying the Footer.
+ *
+ *	@package WordPress
+ *	@package MinimalZerif
+ */
+?>		
+		<footer id="footer">
+			<div class="container">
+				<div class="zerif-copyright-box">
+					Copyright © 2017 <a href="http://qualityassuranceteam.com/" title="QualityAssuranceTeam" target="_blank" rel="nofollow">QualityAssuranceTeam</a>.
+				</div> <!--/END COPYRIGHT-->
+			</div> <!-- / END CONTAINER -->
+		</footer> <!-- / END FOOOTER  -->
+	<?php wp_footer(); ?>
+	</body>
+</html>
